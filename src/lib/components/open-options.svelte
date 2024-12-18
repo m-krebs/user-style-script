7af5263f36efefaab25c316d6a69a74626299a81
Michael Krebs <michikrbs@gmail.com>
2024-12-18 13:05:12 +0100

    refactor: use browser api to open options

--- src/lib/components/open-options.svelte
+++ src/lib/components/open-options.svelte
@@ -3,26 +3,15 @@
   import * as Tooltip from '$lib/components/ui/tooltip';
   import { Settings } from 'lucide-svelte';
 
-  const openOrFocusOptions = () => {
-    const optionsUrl = browser.runtime.getURL('/options.html');
-
-    browser.tabs.query({}, (tabs) => {
-      const existingTab = tabs.find((tab) => tab.url!.startsWith(optionsUrl));
-
-      if (existingTab) {
-        browser.tabs.update(existingTab.id as number, { active: true });
-      } else {
-        browser.tabs.create({ url: optionsUrl });
-      }
-      window.close();
-    });
-  };
+  function openOptions() {
+    browser.runtime.openOptionsPage();
+  }
 </script>
 
 <Tooltip.Provider>
   <Tooltip.Root>
     <Tooltip.Trigger>
-      <Button onclick={openOrFocusOptions} variant="outline" size="icon">
+      <Button onclick={openOptions} variant="outline" size="icon">
         <Settings />
       </Button>
     </Tooltip.Trigger>
