a7f83b9b1b37d5286f7c31eb62a74c32a243b1ca
Michael Krebs <michikrbs@gmail.com>
2025-07-02 00:52:43 +0200

    feat: add biome

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'wxt';
-import path from 'path'
+import path from 'node:path'
+import { defineConfig } from 'wxt'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
     permissions: ['storage', 'tabs', 'userScripts', 'scripting'],
-    host_permissions: ['<all_urls>']
+    host_permissions: ['<all_urls>'],
   },
   manifestVersion: 3,
   srcDir: 'src',
   modules: ['@wxt-dev/module-svelte'],
   alias: {
-    $lib: path.resolve("./src/lib/"),
-    $assets: path.resolve("./src/assets/")
+    $lib: path.resolve('./src/lib/'),
+    $assets: path.resolve('./src/assets/'),
   },
   vite: () => ({
-    resolve: { conditions: ["browser"] }
+    resolve: { conditions: ['browser'] },
   }),
-});
+})
