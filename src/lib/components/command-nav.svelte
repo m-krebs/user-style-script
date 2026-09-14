bb5463aa05ce377c2d85b013b4c453c57bac0205
Michael Krebs <michikrbs@gmail.com>
2026-09-14 23:10:11 +0200

    feat: improve command palette UX
    Reorder groups so rulesets lead, close the palette after selecting an
    item, and add a Cmd/Ctrl+, shortcut to jump straight to settings.

--- src/lib/components/command-nav.svelte
+++ src/lib/components/command-nav.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import * as Command from '$lib/components/ui/command/index.js';
-  import { Puzzle, ScrollText, SettingsIcon } from 'lucide-svelte';
+  import { Puzzle, ScrollText, SettingsIcon } from '@lucide/svelte';
 
   let open = $state(false);
 
@@ -9,38 +9,24 @@
       e.preventDefault();
       open = !open;
     }
+  if (e.key === ',' && (e.metaKey || e.ctrlKey)) {
+      e.preventDefault();
+      window.location.href = '#/settings'
+    }
   }
 </script>
 
 <svelte:document onkeydown={handleKeydown} />
-
-<Command.Dialog bind:open>
+ 
+<Command.Dialog bind:open class="animate-in blur-in-3xl duration-150">
   <Command.Input placeholder="Type a command or search..." />
   <Command.List>
     <Command.Empty>No results found.</Command.Empty>
-    <Command.Group heading="Links">
-      <Command.Item
-        onSelect={() => {
-          window.location.href = '#/modules';
-        }}
-      >
-        <Puzzle class="mr-2 size-4" />
-        <span>External Modules</span>
-      </Command.Item>
-    </Command.Group>
-    <Command.Separator />
-    <Command.Group heading="Settings">
-      <Command.Item>
-        <SettingsIcon class="mr-2 size-4" />
-        <span>Settings</span>
-        <Command.Shortcut>⌘S</Command.Shortcut>
-      </Command.Item>
-    </Command.Group>
-    <Command.Separator />
     <Command.Group heading="Rulesets">
       <Command.Item
         onSelect={() => {
           window.location.href = '#';
+          open = false
         }}
       >
         <ScrollText class="mr-2 size-4" />
@@ -49,11 +35,35 @@
       <Command.Item
         onSelect={() => {
           window.location.href = '#/ruleset/add';
+          open = false
         }}
       >
         <ScrollText class="mr-2 size-4" />
         <span>Rulesets > Create new</span>
       </Command.Item>
     </Command.Group>
+    <Command.Group heading="Links">
+      <Command.Item
+        onSelect={() => {
+          window.location.href = '#/modules';
+          open = false
+        }}
+      >
+        <Puzzle class="mr-2 size-4" />
+        <span>External Modules</span>
+      </Command.Item>
+    </Command.Group>
+    <Command.Separator />
+    <Command.Group heading="Settings">
+      <Command.Item onSelect={()=>{
+        window.location.href = '#/settings'
+        open = false
+      }}>
+        <SettingsIcon class="mr-2 size-4" />
+        <span>Settings</span>
+        <Command.Shortcut>⌘+,</Command.Shortcut>
+      </Command.Item>
+    </Command.Group>
+    <Command.Separator />
   </Command.List>
 </Command.Dialog>
